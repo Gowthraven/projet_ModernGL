4dd157a5fda68e04755950e5bdedf1439331401c
Bains Anaïs <otaku.anais@gmail.com>
2024-03-14 13:59:17 +0100

    ajout début de texture

--- render/texture.py
+++ render/texture.py
@@ -14,10 +14,16 @@ class Texture:
 
     def get_texture(self, path):
         texture = pg.image.load(path).convert()
-        texture = pg.transform.flip(texture, flip_x=False, flip_y=True)
-        texture = self.ctx.texture(size=texture.get_size(), components=3,
-                                   data=pg.image.tostring(texture, 'RGB'))
+        texture = pg.transform.flip(texture, flip_x=False, flip_y=True) #flip the image
+        texture = self.contexte.texture(size=texture.get_size(), components=3,
+                                   data=pg.image.tostring(texture, 'RGB')) #convert the image to a texture
         
+        # AF
+        texture.anisotropy = 32.0 
+        # mipmaps
+        texture.filter = (mg.LINEAR_MIPMAP_LINEAR, mg.LINEAR) #minification, magnification
+        texture.build_mipmaps() #génère les mipmaps
+
         return texture
 
     def destroy(self):
