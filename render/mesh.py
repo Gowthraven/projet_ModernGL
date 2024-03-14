9e957f2138564ee6cd0d508ec7901c1f44c5bcdc
Bains Anaïs <otaku.anais@gmail.com>
2024-03-14 19:44:32 +0100

    ajout classe mesh

---
+++ render/mesh.py
@@ -0,0 +1,13 @@
+from vao import VAO
+from texture import Texture
+
+class Mesh: 
+    def __init__(self, window): 
+        self.window = window
+        self.contexte = window.contexte
+        self.vao = VAO(self.contexte)
+        self.texture = Texture(window)
+
+    def destroy(self): 
+        self.vao.destroy()
+        self.texture.destroy()
