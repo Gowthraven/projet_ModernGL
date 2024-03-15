2879f50a93252964f961526a14e93c76b2066da9
Bains Anaïs <otaku.anais@gmail.com>
2024-03-15 11:10:03 +0100

    maj lumière

--- scene/light.py
+++ scene/light.py
@@ -1,17 +1,17 @@
-import glm 
+import glm
 
 class Light:
-    def __init__(self, position=(1,1,-1), color=(1,1,1)):
-        self.position = glm.vec3(position) #Position de la lumière
-        self.direction = glm.vec3(0,0,0) #Direction de la lumière
-        self.color = glm.vec3(color) #Couleur de la lumière
+    def __init__(self, position=(50, 50, -10), color=(1, 1, 1)):
+        self.position = glm.vec3(position)
+        self.color = glm.vec3(color)
+        self.direction = glm.vec3(0, 0, 0)
 
-        self.I_ambiant = 0.1 * self.color #intensité ambiante
-        self.I_diffuse = 0.8 * self.color #intensité diffuse
-        self.I_specular = 0.9 * self.color #intensité spéculaire
-
-        self.m_view_light = self.get_view_matrix()
+        self.I_ambiant = 0.06 * self.color  # Intensité ambiante
+        self.I_diffuse = 0.75 * self.color  # Intensité diffuse
+        self.I_specular = 1.0 * self.color  # Intensité spéculaire
+        
+        self.view_light = self.get_view_matrix()
 
     def get_view_matrix(self):
-        """Return the view matrix."""
-        return glm.lookAt(self.position, self.direction, glm.vec3(0,1,0)) #glm.lookAt(eye, center, up)
+        return glm.lookAt(self.position, self.direction, glm.vec3(0, 1, 0))
+    
